d1ed67ab64a8d24b9c20d11244a1ee453ceeb249
srujanjoshi <srujanjoshi7@gmail.com>
2021-11-05 01:57:06 -0400

    O(n) solution?

--- meetingmerge/mergemeetings.py
+++ meetingmerge/mergemeetings.py
@@ -2,7 +2,7 @@
 
  # Output Format: [(0, 1), (3, 8), (9, 12)]
 
-def merge_meetings(meeting_times):
+def merge_meetings_on2(meeting_times):
 
     for time_block_a in meeting_times:
         for time_block_b in [x for x in meeting_times if x!= time_block_a]:
@@ -13,4 +13,16 @@ def merge_meetings(meeting_times):
                 meeting_times.append(new_time_block)
     print(meeting_times)
 
+
+def merge_meetings(meeting_times):
+    meeting_times.sort(key= lambda a: a[0])
+    for i in range(len(meeting_times)):
+        if(i< len(meeting_times) and (i+1 < len(meeting_times))):
+            if(meeting_times[i][1]>= meeting_times[i+1][0]):
+                new_time_block= (meeting_times[i][0],max(meeting_times[i][1],meeting_times[i+1][1]))
+                meeting_times[i]=new_time_block
+                meeting_times.remove(meeting_times[i+1])
+    print(meeting_times)
 merge_meetings([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)])
+
+
