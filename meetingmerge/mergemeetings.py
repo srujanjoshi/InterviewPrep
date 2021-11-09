511d892f0bb6bec3648470194255d894548778f6
Srujan Joshi <srujanjoshi7@gmail.com>
2021-11-09 15:05:23 -0500

    Removed incorrect O(n^2) solution

--- meetingmerge/mergemeetings.py
+++ meetingmerge/mergemeetings.py
@@ -2,16 +2,6 @@
 
  # Output Format: [(0, 1), (3, 8), (9, 12)]
 
-def merge_meetings_on2(meeting_times):
-
-    for time_block_a in meeting_times:
-        for time_block_b in [x for x in meeting_times if x!= time_block_a]:
-            if(time_block_a[0] <= time_block_b[1]) and (time_block_a[1]>=time_block_b[0]):
-                new_time_block= (min(time_block_a[0],time_block_b[0]),max(time_block_a[1],time_block_b[1]))
-                meeting_times.remove(time_block_a)
-                meeting_times.remove(time_block_b) 
-                meeting_times.append(new_time_block)
-    print(meeting_times)
 
 
 def merge_meetings(meeting_times):
