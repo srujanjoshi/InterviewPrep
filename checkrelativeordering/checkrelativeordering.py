45c5c06d4f5b9ed50075ab3c964b6df27cea511c
Srujan Joshi <srujanjoshi7@gmail.com>
2021-11-11 06:13:42 -0500

    Created twosum.py

--- checkrelativeordering/checkrelativeordering.py
+++ checkrelativeordering/checkrelativeordering.py
@@ -1,4 +1,6 @@
-def checkrelativeordering(list_a, list_b, combined_list): 
+import unittest
+
+def is_first_come_first_served(list_a, list_b, combined_list): 
     list_a_index=0
     list_b_index=0
     combined_list_index=0
@@ -14,7 +16,46 @@ def checkrelativeordering(list_a, list_b, combined_list):
                 combined_list_index+=1
                 continue
         return False
+    if(list_a_index!=len(list_a) or list_b_index!=len(list_b)):
+        return False
     return True 
 
-print(checkrelativeordering([1, 5], [2, 3, 6], [1, 2, 3, 5, 6, 8]))
-    
+# print(is_first_come_first_served([1, 5], [2, 3, 6], [1, 2, 3, 5, 6, 8]))
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_both_registers_have_same_number_of_orders(self):
+        result = is_first_come_first_served([1, 4, 5], [2, 3, 6], [1, 2, 3, 4, 5, 6])
+        self.assertTrue(result)
+
+    def test_registers_have_different_lengths(self):
+        result = is_first_come_first_served([1, 5], [2, 3, 6], [1, 2, 6, 3, 5])
+        self.assertFalse(result)
+
+    def test_one_register_is_empty(self):
+        result = is_first_come_first_served([], [2, 3, 6], [2, 3, 6])
+        self.assertTrue(result)
+
+    def test_served_orders_is_missing_orders(self):
+        result = is_first_come_first_served([1, 5], [2, 3, 6], [1, 6, 3, 5])
+        self.assertFalse(result)
+
+    def test_served_orders_has_extra_orders(self):
+        result = is_first_come_first_served([1, 5], [2, 3, 6], [1, 2, 3, 5, 6, 8])
+        self.assertFalse(result)
+
+    def test_one_register_has_extra_orders(self):
+        result = is_first_come_first_served([1, 9], [7, 8], [1, 7, 8])
+        self.assertFalse(result)
+
+    def test_one_register_has_unserved_orders(self):
+        result = is_first_come_first_served([55, 9], [7, 8], [1, 7, 8, 9])
+        self.assertFalse(result)
+
+    def test_order_numbers_are_not_sequential(self):
+        result = is_first_come_first_served([27, 12, 18], [55, 31, 8], [55, 31, 8, 27, 12, 18])
+        self.assertTrue(result)
+
+unittest.main(verbosity=2)
