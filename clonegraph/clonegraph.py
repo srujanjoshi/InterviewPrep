a4731e70bbcb61295f7ab141ca5fd03887e760db
srujanjoshi <srujanjoshi7@gmail.com>
2022-02-23 07:09:58 -0500

    Clone Graph and Course Scheduling

---
+++ clonegraph/clonegraph.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        
+        old_to_new ={}
+        
+        def dfs(node):
+            if(node in old_to_new):
+                return old_to_new[node]
+            clone = Node(node.val)
+            old_to_new[node] = clone
+            for neighbor in node.neighbors:
+                clone.neighbors.append(dfs(neighbor))
+                
+            return clone
+        
+        return dfs(node) if node else None
